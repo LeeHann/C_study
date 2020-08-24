28a4a68bb13337bd6965cab4ce14a5330f6763be
LeeHann <0104ihn@naver.com>
2020-08-24 11:35:47 +0900

    0824

---
+++ day21/day21/ex03/ex03.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> vect_1;
+
+    vect_1.push_back(1);
+    vect_1.push_back(2);
+    vect_1.push_back(3);
+    vect_1.push_back(4);
+    vect_1.push_back(5);
+
+    std::cout << "Size : " << vect_1.size() << ", capacity : " << vect_1.capacity() << std::endl;
+    std::cout << "MaxSize : " << vect_1.max_size() << std::endl;
+
+    /*
+    std::cout << vect_1[0] << std::endl;
+    std::cout << vect_1[1] << std::endl;
+    std::cout << vect_1[2] << std::endl;
+    std::cout << vect_1[3] << std::endl;
+    std::cout << vect_1[4] << std::endl;
+    */
+
+    for (auto i = vect_1.begin(); i != vect_1.end(); i++)
+    {
+        std::cout << *i << std::endl;
+    }
+   
+    vect_1[0] = 10;
+    std::cout << vect_1[0] << std::endl;
+    std::cout << "use []" << vect_1[0] << std::endl;
+    std::cout << "use at" << vect_1.at(0) << std::endl;
+
+    vect_1.pop_back();
+    vect_1.insert(vect_1.begin(), 20);
+    vect_1.insert(vect_1.begin()+3, 33);
+    vect_1.erase(vect_1.begin() + 2);
+    std::cout << "------------------------------" << std::endl;
+
+    for (auto i = vect_1.begin(); i != vect_1.end(); i++)
+    {
+        std::cout << *i << std::endl;
+    }
+
+    vect_1.erase(vect_1.end()-1); // == vect_1.pop_back();
+
+    std::cout << "------------------------------" << std::endl;
+
+    for (auto i = vect_1.begin(); i != vect_1.end(); i++)
+    {
+        std::cout << *i << std::endl;
+    }
+
+    return 0;
+}
---
+++ day21/day21/ex04/ex04.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+//	int _a1 = { 7, 4, 9, 3, 5, 1 };
+	std::vector<int> v1;
+
+	for (int i = 0; i < 10; i++)
+	{
+		v1.push_back(i);
+	}
+	for (int i = 0; i < v1.size(); i++)
+	{
+		std::cout << v1[i] << std::endl;
+	}
+
+	v1.resize(5);
+	for (int i = 0; i < v1.size(); i++)
+	{
+		std::cout << v1[i] << std::endl;
+	}
+
+	std::cout << "capacity : " << v1.capacity() << std::endl;
+	v1.shrink_to_fit();
+	std::cout << "capacity : " << v1.capacity() << std::endl;
+
+	return 0;
+}
