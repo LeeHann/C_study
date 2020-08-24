91ddfc0cca82cee08f55f373d6579ca880a4a129
LeeHann <0104ihn@naver.com>
2020-08-24 10:54:02 +0900

    0824

---
+++ day21/day21/ex02/ex02.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+int main()
+{
+    std::string str1 = "hello string";
+    std::string str2 = "30";
+    int myint;
+    int mynum = 40;
+    float _pi = 3.14;
+
+    std::stringstream(str2) >> myint; // string -> int
+    myint++;
+
+    str1 = str1 + std::to_string(mynum) + "," + std::to_string(_pi); // int, float -> string
+
+    std::cout << str1 << "," << myint << std::endl;
+   
+    return 0;
+}
+
