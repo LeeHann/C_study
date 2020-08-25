1cb91c263d0495641d905fffcff20caceff89963
LeeHann <0104ihn@naver.com>
2020-08-25 11:17:33 +0900

    0825

---
+++ day22/ex01/ex01.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+void test(int a, int b)
+{
+	std::cout << a + b << std::endl;
+}
+
+int main()
+{
+	int a = 100;
+	int b[5] = { 1,2,3,4,5 };
+	char* ptr1 = (char *)b;
+	unsigned short* ptr2 = (unsigned short *)b;
+
+	test(a, b[2]);
+
+	return 0;
+}
+
